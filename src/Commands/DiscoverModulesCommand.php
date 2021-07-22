bd07ef786e298809f13176e4e15a3c68f5fc3fe6
Steve McDougall <juststevemcd@gmail.com>
2021-07-22 21:42:31 +0100

    Having to live with issue for now

--- src/Commands/DiscoverModulesCommand.php
+++ src/Commands/DiscoverModulesCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace JustSteveKing\Laravel\ERP\Commands;
 
-use Composer\InstalledVersions;
 use Illuminate\Console\Command;
 use JustSteveKing\Laravel\ERP\Actions\SyncModuleWithStorage;
 
